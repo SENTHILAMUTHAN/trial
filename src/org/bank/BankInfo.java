a43c182b459ac88fae8bf450b30671d45a0714e1
SENTHILAMUTHAN <senthilamuthan97@gmail.com>
2024-07-25 08:44:21 +0530

    changes made in Bankinfo

--- src/org/bank/BankInfo.java
+++ src/org/bank/BankInfo.java
@@ -1,24 +1,23 @@
 package org.bank;
 
-public class BankInfo{
-	
-	
+public class BankInfo {
+
 	public void saving() {
 		// TODO Auto-generated method stub
 		System.out.println("Savings Account");
 
 	}
-	public void fixed() {
-		// TODO Auto-generated method stub
-		System.out.println("Fixed deposit");
 
-	}
+	
+	  public void fixed() { // TODO Auto-generated method stub
+	  System.out.println("Fixed deposit");
+	  
+	  }
+	 
 	public void deposit() {
 		// TODO Auto-generated method stub
-		System.out.println("Types of Deposit provided by Axis bank\t:"+"\n");
+		System.out.println("Types of Deposit provided by Axis bank\t:" + "\n");
 
 	}
-	
 
-	
 }
